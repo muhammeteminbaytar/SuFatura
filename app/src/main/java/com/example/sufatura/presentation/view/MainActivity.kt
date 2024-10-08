d2e1c01627413aeb2b6be78326dd2b922dfac319
muhammeteminbaytar <muhammet2000byt@gmail.com>
2024-10-08 18:34:35 +0300

    added bottom bar

--- app/src/main/java/com/example/sufatura/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/sufatura/presentation/view/MainActivity.kt
@@ -5,17 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.sufatura.R
+import com.example.sufatura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(activityMainBinding.navHostFragment.id) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        activityMainBinding.bottomBar.setupWithNavController(navController)
+
     }
 }
